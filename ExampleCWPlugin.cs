c7872eb6c34bd28ddaa0735bcf620d73fbc2b062
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2024-12-30 18:03:24 +0000

    CRLF fix one last time?

--- ExampleCWPlugin.cs
+++ ExampleCWPlugin.cs
@@ -13,13 +13,13 @@ namespace ExampleCWPlugin;
 // The first argument is the GUID for this mod. This must be globally unique across all mods.
 // Consider prefixing your name/etc. to the GUID. (or generate an actual GUID)
 [ContentWarningPlugin("ExampleCWPlugin", "0.1", false)]
-public class ExamplePlugin
+public class ExampleCWPlugin
 {
-    static ExamplePlugin()
+    static ExampleCWPlugin()
     {
         // Static constructors of types marked with ContentWarningPluginAttribute are automatically invoked on load.
         // Register callbacks, construct stuff, etc. here.
-        Debug.Log("Hello from ExamplePlugin! This is called on plugin load");
+        Debug.Log("Hello from ExampleCWPlugin! This is called on plugin load");
         // Adding the [ContentWarningSetting] attribute to a setting class is basically the same as:
         // GameHandler.Instance.SettingsHandler.AddSetting(new ExampleSetting());
     }
