00e52de59ae6a504af9ace67b810d37c01baa9af
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2024-12-30 18:35:57 +0000

    Overall code reformat, README.md reformat, and use of AssemblyPublicizer to kill off ugly Reflection code (and preview.png upd'd!) (#1)

--- ExampleCWPlugin.cs
+++ ExampleCWPlugin.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using Unity.Mathematics;
 using UnityEngine;
 using Zorro.Settings;
@@ -13,9 +13,9 @@ namespace ExampleCWPlugin;
 // The first argument is the GUID for this mod. This must be globally unique across all mods.
 // Consider prefixing your name/etc. to the GUID. (or generate an actual GUID)
 [ContentWarningPlugin("ExampleCWPlugin", "0.1", false)]
-public class PluginTy
+public class ExampleCWPlugin
 {
-    static PluginTy()
+    static ExampleCWPlugin()
     {
         // Static constructors of types marked with ContentWarningPluginAttribute are automatically invoked on load.
         // Register callbacks, construct stuff, etc. here.
@@ -28,18 +28,30 @@ public class PluginTy
 // Harmony patches are automatically applied by the vanilla modloader.
 // If 0Harmony.dll is already loaded by BepInEx, then BepInEx's harmony will be used instead.
 [HarmonyPatch(typeof(Flashlight))]
-[HarmonyPatch("Update")]
-public class Patch
+public class FlashlightPatches
 {
-    static AccessTools.FieldRef<Flashlight, BatteryEntry> batteryEntry =
-        AccessTools.FieldRefAccess<Flashlight, BatteryEntry>("m_batteryEntry");
-    static ExampleSetting? exampleSetting;
-
-    static bool Prefix(Flashlight __instance)
+    private static ExampleSetting? exampleSetting;
+    
+    // Be careful when patching methods, as, if you don't have the correct method name, then Harmony will fail to find it.
+    // This can be done in two ways:
+    // a) Use nameof() to get the method name.
+    //  This is safer than using strings as, if the method name changes in a game update, the compiler
+    //  will catch it and your IDE will display it in red.
+    //
+    // b) Use strings, but make sure to update them if the method name changes.
+    //  Harmony, as said before, will fail to find the method if you do not!
+    //  This is why it's recommended to use nameof() instead.
+    [HarmonyPatch(nameof(Flashlight.Update))]
+    [HarmonyPrefix]
+    private static bool UpdatePrefix(Flashlight __instance)
     {
         exampleSetting ??= GameHandler.Instance.SettingsHandler.GetSetting<ExampleSetting>();
-        var bat = batteryEntry(__instance);
+
+        // This is where BepInEx's AssemblyPublicizer comes in handy!
+        // m_batteryEntry would usually be private, but we can access it.
+        var bat = __instance.m_batteryEntry;
         bat.m_charge = Mathf.Max(bat.m_charge, bat.m_maxCharge * (exampleSetting.Value / 100));
+        
         return true;
     }
 }
@@ -48,9 +60,12 @@ public class Patch
 [ContentWarningSetting]
 public class ExampleSetting : FloatSetting, IExposedSetting {
     public override void ApplyValue() => Debug.Log($"omg, mod setting changed to {Value}");
-    protected override float GetDefaultValue() => 100;
-    protected override float2 GetMinMaxValue() => new(0, 100);
+    
+    public override float GetDefaultValue() => 100;
+    public override float2 GetMinMaxValue() => new(0, 100);
+    
     // Prefer using the Mods category
     public SettingCategory GetSettingCategory() => SettingCategory.Mods;
+    
     public string GetDisplayName() => "Example mod setting";
 }
